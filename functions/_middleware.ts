2690c9d068559715d124c8983d8cc0b0be177bf0
Willin Wang <willin@willin.org>
2022-01-02 14:58:25 +0800

    chore: :test_tube: test env setting

--- functions/_middleware.ts
+++ functions/_middleware.ts
@@ -1,9 +1,10 @@
 /* eslint-disable */
+const env = NODE_ENV as string;
+
 export async function onRequest(context) {
   context.data.timestamp = Date.now();
   const res = await context.next().catch((e) => {
-    console.error(e);
-    return new Response(e, { status: 500 });
+    return new Response(env === 'preview' ? e : 'Opps!', { status: 500 });
   });
   const delta = Date.now() - context.data.timestamp;
   res.headers.set('x-response-timing', delta);
