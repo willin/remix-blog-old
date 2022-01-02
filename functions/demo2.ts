a1e06eac4c6d6f712fc3dd4b4462c3c96dec7dcc
Willin Wang <willin@willin.org>
2022-01-02 14:56:36 +0800

    chore: :test_tube: add kvnamespace test

---
+++ functions/demo2.ts
@@ -0,0 +1,14 @@
+export const onRequestGet = async () => {
+  const total = await (views as KVNamespace).get('total').catch((e) => e);
+  const data = {
+    hello: 'world',
+    total
+  };
+  const json = JSON.stringify(data, null, 2);
+
+  return new Response(json, {
+    headers: {
+      'content-type': 'application/json;charset=UTF-8'
+    }
+  });
+};
