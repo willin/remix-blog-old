cfd1ded3821b78f18ef67440dcee1570d4c5247b
Willin Wang <willin@willin.org>
2022-01-02 15:09:24 +0800

    fix: :test_tube: fix env settings

--- functions/_middleware.ts
+++ functions/_middleware.ts
@@ -1,10 +1,8 @@
 /* eslint-disable */
-const env = NODE_ENV as string;
-
 export async function onRequest(context) {
   context.data.timestamp = Date.now();
   const res = await context.next().catch((e) => {
-    return new Response(env === 'preview' ? e : 'Opps!', { status: 500 });
+    return new Response(e, { status: 500 });
   });
   const delta = Date.now() - context.data.timestamp;
   res.headers.set('x-response-timing', delta);
