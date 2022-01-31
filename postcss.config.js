f65dcb142d93b3cd577ddba291c8e5dec1725292
Willin Wang <willin@willin.org>
2022-01-31 10:32:35 +0800

    docs: :memo: update posts

--- postcss.config.js
+++ postcss.config.js
@@ -1,15 +1,6 @@
 module.exports = {
   plugins: {
     tailwindcss: {},
-    autoprefixer: {},
-    '@fullhuman/postcss-purgecss': {
-      content: [
-        './app/**/*.{js,jsx,ts,tsx}',
-        //
-        './content/**/*.{js,jsx,ts,tsx,mdx}'
-      ],
-      defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
-      safelist: ['html', 'body', /data-theme$/]
-    }
+    autoprefixer: {}
   }
 };
