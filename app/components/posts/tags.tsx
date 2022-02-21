3cf17827a650346cd0a56e7e697e8617d803620e
Willin Wang <willin@willin.org>
2022-02-21 11:40:08 +0800

    docs: 📝  update contents  (#15)
    * fix: :bug: fix i18n bug
    
    * docs: :memo: update playground
    
    * docs: :memo: update pages

--- app/components/posts/tags.tsx
+++ app/components/posts/tags.tsx
@@ -1,5 +1,6 @@
 import clsx from 'classnames';
 import { Link } from 'remix';
+import { useI18n } from 'remix-i18n';
 
 const classNames = [
   'btn-primary',
@@ -12,11 +13,13 @@ const classNames = [
 ];
 
 export function TagList({ tags }: { tags: [string, number][] }) {
+  const i18n = useI18n();
+
   return (
     <div className='my-2 flex flex-wrap items-center flex-row'>
       {tags.map(([tag, count], i) => (
         <Link
-          to={`/posts?search=${encodeURIComponent(tag)}`}
+          to={`/${i18n.locale()}/posts?search=${encodeURIComponent(tag)}`}
           className={clsx('btn btn-sm m-2', classNames[i % classNames.length])}
           key={`${tag}-${count}`}>
           {tag}
